9bc494945b60c183a87cb358762b1f14502b85be
ananthan97 <harikumar.a@northeastern.edu>
2022-10-02 01:36:26 +0530

    first_frame

---
+++ JavaApplication6/src/model/Employee.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author Ananthakrishnan H
+ */
+public class Employee {
+    private String Name;
+    private String EmployeeID;
+    private int Age;
+    private String Gender;
+    private String StartDate;
+    private String Level;
+    private String TeamInfo;
+    private String PositionTitle;
+    private String CellPhoneNumber;
+    private Image Photo;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getEmployeeID() {
+        return EmployeeID;
+    }
+
+    public void setEmployeeID(String EmployeeID) {
+        this.EmployeeID = EmployeeID;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(String StartDate) {
+        this.StartDate = StartDate;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public String getTeamInfo() {
+        return TeamInfo;
+    }
+
+    public void setTeamInfo(String TeamInfo) {
+        this.TeamInfo = TeamInfo;
+    }
+
+    public String getPositionTitle() {
+        return PositionTitle;
+    }
+
+    public void setPositionTitle(String PositionTitle) {
+        this.PositionTitle = PositionTitle;
+    }
+
+    public String getCellPhoneNumber() {
+        return CellPhoneNumber;
+    }
+
+    public void setCellPhoneNumber(String CellPhoneNumber) {
+        this.CellPhoneNumber = CellPhoneNumber;
+    }
+
+    public Image getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(Image Photo) {
+        this.Photo = Photo;
+    }
+    
+}
---
+++ JavaApplication6/src/model/Employees.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ananthakrishnan H
+ */
+public class Employees {
+    private ArrayList<Employee> EmployeeList;
+    
+    public Employees(){
+        this.EmployeeList = new ArrayList<Employee>();
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return EmployeeList;
+    }
+
+    public void setEmployeeList(ArrayList<Employee> EmployeeList) {
+        this.EmployeeList = EmployeeList;
+    }
+    
+    public Employee addNewEmployee(){
+        Employee newEmployee = new Employee();
+        EmployeeList.add(newEmployee);
+        return newEmployee;
+    }
+}
